0d31517ba7e158ce6e285c3880d6d98241e671c0
ngcdat <ngcdat@vnpt.vn>
2026-04-06 00:08:03 +0700

    init project

---
+++ src/test/java/com/tutien/pixel/PixelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tutien.pixel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PixelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
